921c39d92787f54be706b05874d2d95f95734c61
Doug <124453486+takilesi@users.noreply.github.com>
2023-07-19 17:31:22 -0400

    it works! all my own logicgit add -A

--- app.js
+++ app.js
@@ -1,47 +1,56 @@
-let operatorsAndNumbers = []; 
+let opsNums = []; 
 total = 0
+tempNum = ""
 
 let nums = document.querySelectorAll(".buttonNumbers");
 for (i of nums) {
     i.addEventListener('click', function() {
-        document.querySelector(".screen").innerHTML+=this.innerHTML; 
-        operatorsAndNumbers.push(this.innerHTML);
-  });
+        document.querySelector(".screen").innerHTML+=this.innerHTML;
+        tempNum += "" + this.innerHTML
+
+    });
 }
 
 let ops = document.querySelectorAll(".fourOperations");
 for (i of ops) {
     i.addEventListener('click', function() {
         document.querySelector(".screen").innerHTML+=this.innerHTML; 
-        operatorsAndNumbers.push(this.innerHTML);
+        total = parseInt(tempNum)
+        opsNums.push(total);
+        opsNums.push(this.innerHTML);
+        tempNum = 0; 
   });
 }
 
 const ac = () => {
     document.querySelector(".screen").innerHTML="";
-    operatorsAndNumbers = []; 
+    opsNums = []; 
     total = 0; 
+    tempNum = 0; 
 }
 
 const calculate = () => {
 
-    total = parseInt(operatorsAndNumbers[0])
-    for (i=0; i <  operatorsAndNumbers.length-1; i++) {
-        if (operatorsAndNumbers[i] == "/" || 
-            operatorsAndNumbers[i] == "+" ||
-            operatorsAndNumbers[i] == "-" ||
-            operatorsAndNumbers[i] == "*"  ) {
-                if(operatorsAndNumbers[i] == "/"){
-                    total = total / parseInt(operatorsAndNumbers[i+1])
+    total = parseInt(tempNum)
+    opsNums.push(total);
+
+    total = parseInt(opsNums[0])
+    for (i=0; i <  opsNums.length-1; i++) {
+        if (opsNums[i] == "/" || 
+            opsNums[i] == "+" ||
+            opsNums[i] == "-" ||
+            opsNums[i] == "*"  ) {
+                if(opsNums[i] == "/"){
+                    total = total / parseInt(opsNums[i+1])
                 }
-                else if(operatorsAndNumbers[i] == "+"){
-                    total = total + parseInt(operatorsAndNumbers[i+1])
+                else if(opsNums[i] == "+"){
+                    total = total + parseInt(opsNums[i+1])
                 }
-                else if(operatorsAndNumbers[i] == "-"){
-                    total = total - parseInt(operatorsAndNumbers[i+1])
+                else if(opsNums[i] == "-"){
+                    total = total - parseInt(opsNums[i+1])
                 }
-                else if(operatorsAndNumbers[i] == "*"){
-                    total = total * parseInt(operatorsAndNumbers[i+1])
+                else if(opsNums[i] == "*"){
+                    total = total * parseInt(opsNums[i+1])
                 }
             }
     }
